29048ed474a70b9e2c9bfb34116ba269cd6d912f
viniciusmdev <contato.vinicius93@gmail.com>
2019-04-06 15:24:58 -0300

    Projeto iniciado

---
+++ projeto-teste/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace projeto_teste {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Olá mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
